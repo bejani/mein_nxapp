0c80cfd2c50a3aaed6b5dc1fc71422e942764b2a
bejani <bejani@gmail.com>
2024-09-12 00:09:59 +0330

    commit 3 ignore iteration.

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,16 @@ export default async function Home() {
   return (
     <div>
     <h1>File Content</h1>
-    <pre>{data.map(rec=>{
+    <div>{data[0].name}</div>
+    <div>{data[1].name}</div>
+    
+    {/* <pre>{data.map(rec=>{
       return (
         <div>
         <div key={rec.id} >{rec.name}</div>
         </div>
       )
-    })}</pre>
+    })}</pre> */}
   </div>
   );
 }
